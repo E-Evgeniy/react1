e54fc206a4cd9eaa5545cb8d0e076c4db9c2efd9
Evgeniy-ES <ees86@yandex.ru>
2023-02-10 13:00:21 +0300

    end

--- app/javascript/bundles/App/components/Dialogs/Dialogs.jsx
+++ app/javascript/bundles/App/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,28 @@ import s from './Dialogs.module.css';
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+          <div className={s.dialogsItems}>
+            <div className={s.dialogs + ' ' + s.active}>
+                Peter
+            </div>
+            <div className={s.dialogs}>
+                Ivan
+            </div>
+            <div className={s.dialogs}>
+                Eva
+            </div>
+            <div className={s.dialogs}>
+                Anna
+            </div>
+          </div>
+
+          <div className={s.messages}>
+            <div className={s.message}>Hi</div>
+            <div className={s.message}>Hjw are you?</div>
+            <div className={s.message}>Bye</div>
+
+          </div>
         </div>
     )
 }

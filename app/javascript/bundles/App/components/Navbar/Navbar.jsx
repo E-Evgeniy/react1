024d003dca121866cd089db4ae7eeef4622be5bd
Evgeniy-ES <ees86@yandex.ru>
2023-02-10 10:23:16 +0300

    end

--- app/javascript/bundles/App/components/Navbar/Navbar.jsx
+++ app/javascript/bundles/App/components/Navbar/Navbar.jsx
@@ -1,27 +1,31 @@
 import React from 'react';
 import s from './Navbar.module.css';
+import { NavLink } from 'react-router-dom';
+
+const setActive = (({ isActive }) =>
+isActive ? s.active : undefined)
 
 const Navbar = () => {
    return <nav className={s.nav}>
 
    <div className={s.item}>
-     <a href="/profile"> Profile </a>
+     <NavLink to="/profile" className={setActive}> Profile </NavLink>
    </div>
 
    <div className={`${s.item} ${s.active}`}>
-     <a href="/dialogs"> Messages </a>
+     <NavLink to="/dialogs" className={setActive}> Messages </NavLink>
    </div>
 
    <div className={s.item}>
-     <a href="/news"> News </a>
+     <NavLink to="/news" className={setActive}> News </NavLink>
    </div>
 
    <div className={s.item}>
-     <a href="/music"> Music </a>
+     <NavLink to="/music" className={setActive}> Music </NavLink>
    </div>
 
    <div className={s.item}>
-     <a href="/settings"> Settings </a>
+     <NavLink to="/settings" className={setActive}> Settings </NavLink>
    </div>
  </nav>
 }
